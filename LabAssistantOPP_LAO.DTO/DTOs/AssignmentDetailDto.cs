aefb476e52ccc617109c79d144903ad87ae6e2fd
PhucKhai2864004 <161446888+PhucKhai2864004@users.noreply.github.com>
2025-06-13 00:02:43 +0700

    Thêm chức năng cho Student
     xem danh sách bài lab, xem chi tiết từng bài, nộp file zip, xem những bài đã nộp và xem số điểm loc hiện tại

---
+++ LabAssistantOPP_LAO.DTO/DTOs/AssignmentDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAssistantOPP_LAO.DTO.DTOs
+{
+    public class AssignmentDetailDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int? LocTotal { get; set; }
+    }
+}
+
+
