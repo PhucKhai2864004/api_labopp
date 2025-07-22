efd56938896aeb28d02d3b5228216ef1b7c80ac0
PhucKhai2864004 <161446888+PhucKhai2864004@users.noreply.github.com>
2025-07-22 15:30:28 +0700

    Update Program.cs

--- LabAssistantOPP_LAO.WebApi/Program.cs
+++ LabAssistantOPP_LAO.WebApi/Program.cs
@@ -114,11 +114,10 @@ builder.WebHost.ConfigureKestrel(serverOptions =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
 	app.UseSwagger();
 	app.UseSwaggerUI();
-}
+
 
 app.UseCors("AllowFrontend");
 
