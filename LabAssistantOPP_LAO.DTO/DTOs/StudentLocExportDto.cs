35f7b0e79127c332a153f2face6574b7c67adaa1
PhucKhai2864004 <161446888+PhucKhai2864004@users.noreply.github.com>
2025-08-26 23:19:23 +0700

    thêm export điểm cho 1 lớp

---
+++ LabAssistantOPP_LAO.DTO/DTOs/StudentLocExportDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAssistantOPP_LAO.DTO.DTOs
+{
+    public class StudentLocExportDto
+    {
+        public string StudentCode { get; set; }
+        public string StudentName { get; set; }
+        public int TotalLoc { get; set; }
+        public int Status { get; set; }   // 0 = not passed, 1 = passed
+    }
+}
