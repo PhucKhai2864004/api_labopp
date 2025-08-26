96b2aba7ec7f572cc3ccdfc36d7d0eb5df0fd933
Cosmo-c-bit <dung9v3@gmail.com>
2025-08-26 19:54:15 +0700

    fix head tạo assignment

--- LabAssistantOPP_LAO.DTO/DTOs/LabAssignmentDto.cs
+++ LabAssistantOPP_LAO.DTO/DTOs/LabAssignmentDto.cs
@@ -46,7 +46,9 @@ namespace LabAssistantOPP_LAO.DTO.DTOs
 
         [Required(ErrorMessage = "Trạng thái không được để trống.")]
         public string Status { get; set; }
-    }
+
+		public List<int>? ClassIds { get; set; }
+	}
 
 
 }
